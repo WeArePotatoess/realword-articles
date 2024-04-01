f94fab8d0c14aabed4c907f61300ee89cb193518
WeArePotatoess <shinigamitedomi@gmail.com>
2024-04-01 17:45:22 +0700

    complete home page

--- src/layouts/Home/index.jsx
+++ src/layouts/Home/index.jsx
@@ -15,20 +15,21 @@ const Home = () => {
     const [articles, setArticles] = useState(true);
     const [articlesCount, setArticlesCount] = useState();
     const [offset, setOffset] = useState(0);
-    const [articleTab, setArticleTab] = useState('Your Feed');
     const [tags, setTags] = useState();
-    const [articleTag, setArticleTag] = useState();
+    const [currentFeed, setCurrentFeed] = useState();
     const [cancelToken, setCancelToken] = useState(null);
-
+    const [tagFilter, setTagFilter] = useState();
 
     const handleViewArticles = (eventKey) => {
-        setArticleTab(eventKey);
+        setOffset(0);
+        setCurrentFeed(eventKey);
+        const selectedTag = eventKey.replace('#', '');
         if (cancelToken) cancelToken.cancel('canceled previous request');
         const newCancelToken = axios.CancelToken.source();
         setCancelToken(newCancelToken);
         setArticles([]);
         setLoadingArticles(true);
-        getArticles({ author: eventKey === 'Your Feed' ? user.username : undefined, tag: tags?.includes(eventKey.replace('#', '')) ? eventKey : undefined }, newCancelToken.token)
+        getArticles({ author: eventKey === 'Your Feed' ? user.username : undefined, tag: tags?.includes(selectedTag) ? selectedTag : '' }, newCancelToken.token)
             .then(data => {
                 setArticles(data.articles);
                 setArticlesCount(data.articlesCount);
@@ -40,16 +41,14 @@ const Home = () => {
                 setLoadingArticles(false)
             })
     }
-
     const handleChangePage = (page) => {
-        // console.log(articleTab)
         if (cancelToken) cancelToken.cancel('canceled previous request');
         const newCancelToken = axios.CancelToken.source();
         setCancelToken(newCancelToken);
         setLoadingArticles(true);
         setOffset((page - 1) * getArticlesLimit);
         setArticles([]);
-        getArticles({ author: articleTab === 'Your Feed' ? user.username : undefined, offset: (page - 1) * getArticlesLimit }, newCancelToken.token)
+        getArticles({ author: currentFeed === 'Your Feed' ? user.username : undefined, offset: (page - 1) * getArticlesLimit, tag: currentFeed.replace('#', '') === tagFilter ? tagFilter : undefined }, newCancelToken.token)
             .then(data => {
                 setArticles(data.articles)
                 setLoadingArticles(false)
@@ -60,12 +59,18 @@ const Home = () => {
                 setLoadingArticles(false)
             });
     }
+    const viewArticlesWithTag = (tag) => {
+        setTagFilter(tag);
+        setCurrentFeed(`#${tag}`);
+        handleViewArticles(`#${tag}`);
+    }
 
     useEffect(() => {
         const cancelToken = axios.CancelToken.source();
-        if (!user) setArticleTab('Global Feed');
+        if (!user) setCurrentFeed('Global Feed')
+        else setCurrentFeed('Your Feed')
         setLoadingArticles(true);
-        getArticles({ author: articleTab === 'Your Feed' ? user?.username : undefined }, cancelToken.token)
+        getArticles({ author: user ? user?.username : undefined }, cancelToken.token)
             .then(data => {
                 setArticles(data.articles);
                 setArticlesCount(data.articlesCount);
@@ -81,7 +86,7 @@ const Home = () => {
             .then(data => setTags(data.tags))
             .catch(err => { if (!axios.isCancel(err)) console.log(err) })
         return () => cancelToken.cancel();
-    }, [])
+    }, [user])
 
     return (<>
         {!user && <Banner />}
@@ -89,9 +94,9 @@ const Home = () => {
             <Row className="align-items-start">
                 <Col xs={9}>{user ?
                     <ArticlesNavigator handleViewArticles={handleViewArticles}
-                        tabs={['Your Feed', 'Global Feed']} /> :
+                        tabs={!tagFilter ? ['Your Feed', 'Global Feed'] : ['Your Feed', 'Global Feed', `#${tagFilter}`]} activeKey={currentFeed} /> :
                     <ArticlesNavigator handleViewArticles={handleViewArticles}
-                        tabs={['Global Feed']} />
+                        tabs={!tagFilter ? ['Global Feed'] : ['Global Feed', `#${tagFilter}`]} activeKey={currentFeed} />
                 }
                     <ArticlesList loading={loadingArticles} articles={articles} handleChangePage={handleChangePage} articlesCount={articlesCount} offset={offset} /></Col>
                 <Col className="tag-list border rounded-2 p-2">
@@ -99,7 +104,7 @@ const Home = () => {
                     <div className="d-flex gap-2 flex-wrap">
                         {tags?.map(tag => {
                             return (
-                                <Button key={tag} variant="secondary" className="rounded-5">
+                                <Button onClick={() => viewArticlesWithTag(tag)} key={tag} variant="secondary" className="rounded-5">
                                     {tag}
                                 </Button>
                             )
